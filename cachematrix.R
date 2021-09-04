c49d001a2baf928523fd0725ab1646eef134010e
drmkn <81362311+drmkn@users.noreply.github.com>
2021-09-04 15:11:31 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## The following functions compute the inverse of a matrix. In order to make
+## computations faster the functions avoid repeated calculation of inverse 
+## for the same matrix and instead of that it caches the inverse.
+
+## The following function creates a special type of matrix that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  z <- NULL
+  ## z corresponds to the inverse of matrix x 
+  set <- function(y){
+    x <<- y
+    z <<- NULL
+  } ## Function 'set' resets the matrix x and its inverse. 
+  get <- function() x
+  setinverse <- function(inverse) z <<- inverse
+  getinverse <- function() z
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
+## The following function computes the inverse of the special type of matrix returned by makeCacheMatrix 
+## above. If the inverse has already been calculated (and the matrix has not changed), then the cacheSolve 
+## retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  z <- x$getinverse()
+  if(!is.null(z)) {
+    message("getting cached data")
+    return(z)
+  }## This if condition checks if the inverse of the matrix x is in the cache memory. 
+  data <- x$get() ## if the above if condition is not satisfied then it computes the inverse of matrix x by solve function.
+  z <- solve(data,...)
+  x$setinverse(z)## It computes the inverse and stores it in the cache memory.
+  z
+  ## Return a matrix that is the inverse of 'x'
 }
